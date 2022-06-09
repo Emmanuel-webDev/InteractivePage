a9cb33d4a92ce34b7841b97d331501cfaaedcbf6
Emmanuel Onuorah <98954046+Emmanuel-webDev@users.noreply.github.com>
2022-06-09 21:09:17 +0100

    Add files via upload
    Frontend Mentor Challenge

---
+++ app.js
@@ -0,0 +1,66 @@
+function activatedState(){
+    document.querySelector('a').style.backgroundColor= "hsl(0 0% 100%)"
+    document.querySelector('a').style.color = "hsl(25, 97%, 53%)"
+}
+function unactivatedState(){
+    document.querySelector('a').style.backgroundColor= "hsl(25, 97%, 53%)"
+    document.querySelector('a').style.color = "hsl(0 0% 100%)"
+}
+
+
+
+
+function nxtPage(){
+    document.querySelector('a').href= "Last.html"
+}
+
+
+function btnactivatedState(){
+    document.querySelector(".btn1").style.backgroundColor= "hsl(25, 97%, 53%)"
+    document.querySelector(".btn1").style.color = "hsl(0 0% 100%)"  
+}
+
+function btnunactivatedState(){
+    document.querySelector('.btn1').style.backgroundColor= "hsl(216, 12%, 8%)"
+    document.querySelector('.btn1').style.color = "hsl(217, 12%, 63%)"
+}
+
+function activatedStatetwo(){
+    document.querySelector(".btn2").style.backgroundColor= "hsl(25, 97%, 53%)"
+    document.querySelector(".btn2").style.color = "hsl(0 0% 100%)"  
+}
+
+function unactivatedStatetwo(){
+    document.querySelector('.btn2').style.backgroundColor= "hsl(216, 12%, 8%)"
+    document.querySelector('.btn2').style.color = "hsl(217, 12%, 63%)"
+}
+
+function activatedStatethree(){
+    document.querySelector(".btn3").style.backgroundColor= "hsl(25, 97%, 53%)"
+    document.querySelector(".btn3").style.color = "hsl(0 0% 100%)"  
+}
+
+function unactivatedStatethree(){
+    document.querySelector('.btn3').style.backgroundColor= "hsl(216, 12%, 8%)"
+    document.querySelector('.btn3').style.color = "hsl(217, 12%, 63%)"
+}
+
+function activatedStatefour(){
+    document.querySelector(".btn4").style.backgroundColor= "hsl(25, 97%, 53%)"
+    document.querySelector(".btn4").style.color = "hsl(0 0% 100%)"  
+}
+
+function unactivatedStatefour(){
+    document.querySelector('.btn4').style.backgroundColor= "hsl(216, 12%, 8%)"
+    document.querySelector('.btn4').style.color = "hsl(217, 12%, 63%)"
+}
+
+function activatedStatefive(){
+    document.querySelector(".btn5").style.backgroundColor= "hsl(25, 97%, 53%)"
+    document.querySelector(".btn5").style.color = "hsl(0 0% 100%)"  
+}
+
+function unactivatedStatefive(){
+    document.querySelector('.btn5').style.backgroundColor= "hsl(216, 12%, 8%)"
+    document.querySelector('.btn5').style.color = "hsl(217, 12%, 63%)"
+}
